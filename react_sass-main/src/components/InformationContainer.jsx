a65e5383614b26c17a4b6a13495143e4e2912f18
j3ipy <pedrosant1905@gmail.com>
2022-11-08 10:11:01 -0300

    Projeto em desenvolvimento

---
+++ react_sass-main/src/components/InformationContainer.jsx
@@ -0,0 +1,33 @@
+import "../styles/components/informationcontainer.sass";
+
+import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from "react-icons/ai";
+
+const InformationContainer = () => {
+  return (
+    <section id="information">
+      <div className="info-card">
+        <AiFillPhone id="phone-icon" />
+        <div>
+          <h3>Telefone</h3>
+          <p>(79)99964-1631</p>
+        </div>
+      </div>
+      <div className="info-card">
+        <AiOutlineMail id="email-icon" />
+        <div>
+          <h3>E-mail</h3>
+          <p>designerjp05@gmail.com</p>
+        </div>
+      </div>
+      <div className="info-card">
+        <AiFillEnvironment id="pin-icon" />
+        <div>
+          <h3>Localização</h3>
+          <p>Estância / SE</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default InformationContainer;
