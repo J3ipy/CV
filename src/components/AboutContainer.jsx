8fdd1a89b81500f29578c61207fcd604f47e8bd7
João Pedro <97753966+J3ipy@users.noreply.github.com>
2022-12-06 18:45:07 -0300

    Projeto Finalizado

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,15 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+      Sempre me esforcei para alcançar meus objetivos
+      laborais, visando constantemente o meu
+      aprimoramento profissional.
+
+      </p>
+    </section>
+  );
+};
+
+export default AboutContainer;
---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,18 @@
+const ProjectsContainer = () => {
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        BadgeSpace - ASP .NET Core
+      </p>
+      <p>
+        Landing Pages - HTML5 | CSS3 | JS
+      </p>
+      <a href="https://github.com/J3ipy" className="btn">
+        Ver Projetos
+      </a>
+    </section>
+  );
+};
+
+export default ProjectsContainer;
